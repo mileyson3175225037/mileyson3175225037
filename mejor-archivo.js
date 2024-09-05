7083b40953f9ff91c14fab0a26507d017b5679cb
mileyson nuñez hinestroza <mileyson1995@gmail.com>
2024-09-05 16:44:22 -0500

    cambio mas atualizado

---
+++ mejor-archivo.js
@@ -0,0 +1,30 @@
+/* aqui voy a poner todo el codigo script de el index.html */
+
+/* qui va todo lo que tiene que ver con las flechas del  */
+// Obtener referencias a los elementos
+const scrollContainer = document.getElementById('scrollContainer');
+const arriva = document.getElementById('flecha-arriva');
+const abajo  = document.getElementById('flecha-abajo');
+
+// Definir la cantidad de desplazamiento (en píxeles)
+const scrollAmount = 50;
+
+// Función para desplazarse hacia arriba
+arriva.addEventListener('click', function() {
+    scrollContainer.scrollBy({
+        top: -scrollAmount, // Desplazar hacia arriba
+        behavior: 'smooth'  // Desplazamiento suave
+    });
+});
+
+// Función para desplazarse hacia abajo
+abajo.addEventListener('click', function() {
+    scrollContainer.scrollBy({
+        top: scrollAmount,  // Desplazar hacia abajo
+        behavior: 'smooth'  // Desplazamiento suave
+    });
+});
+
+
+
+/* aqui va a terminar todo el scrip de el archivo html*/
